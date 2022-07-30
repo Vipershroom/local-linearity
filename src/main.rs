3a8697cb9e9f9b3a32165dfa139e269bad9c5bb9
Vipershroom <undertalefan812@gmail.com>
2022-07-30 01:56:31 -0400

    Add clap and support for cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+    #[clap(value_parser)]
+    num: Option<f32>,
 }
+
+fn local_linearity() {
+    
+}
+
+
+fn main() {
+    let args = Args::parse();
+    println!("{:?}", args.num.unwrap())
+}
+
+
+
